d4acfbbccbed4ebd0c95d6f4670d64d8dd845fc2
Rageh97 <abdelhady412rageh@gmail.com>
2023-07-15 15:57:37 +0300

    first commit

---
+++ src/app/Articles/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>articles</h1>
+        
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/Posts/page.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>posts</h1>
+        
+    </div>
+    
+  )
+}
+
+export default page
